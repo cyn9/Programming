d731562043dc10535c15ac5ee08bac17e9183146
cyn9 <cihan.asci@tufts.edu>
2020-10-25 16:01:34 -0400

    Added a few more things.

--- Python/OOP/Example1/User.py
+++ Python/OOP/Example1/User.py
@@ -1,8 +1,11 @@
 class User:
+    activeUsers = 0
+
     def __init__(self, firstName, lastName, age):
         self.firstName = firstName
         self.lastName = lastName
         self.age = age
+        User.activeUsers += 1
     
     def print(self):
         print(f"{self.firstName} {self.lastName}, {self.age}.")
@@ -17,10 +20,21 @@ class User:
         senior = self.age > 65
         return f"{self.initials()} is a senior." if senior else f"{self.initials()} is not a senior."
 
+    def userLogout(self):
+        print(f"{self.fullName()} has logged out.")
+        User.activeUsers -= 1
+
+print(f"Number of users : {User.activeUsers}.")
+
 user1 = User("John", "Doe", 19)
 user1.print()
 print(user1.isSenior())
+print(f"Number of users : {User.activeUsers}.")
 
 user2 = User("Mary", "Jane", 66)
 user2.print()
 print(user2.isSenior())
+print(f"Number of users : {User.activeUsers}.")
+
+user2.userLogout()
+print(f"Number of users : {User.activeUsers}.")
