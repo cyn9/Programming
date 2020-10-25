ae9d50b015f505382b642731002e320ee2b08463
cyn9 <cihan.asci@tufts.edu>
2020-10-25 15:50:43 -0400

    Added another method.

--- Python/OOP/Example1/User.py
+++ Python/OOP/Example1/User.py
@@ -5,7 +5,7 @@ class User:
         self.age = age
     
     def print(self):
-        print(f"{self.firstName} {self.lastName}, {self.age}")
+        print(f"{self.firstName} {self.lastName}, {self.age}.")
 
     def fullName(self):
         return f"{self.firstName} {self.lastName}"
@@ -13,6 +13,14 @@ class User:
     def initials(self):
         return f"{self.firstName[0]}{self.lastName[0]}"
 
+    def isSenior(self):
+        return f"{self.initials()} is a senior." if self.age > 65 else f"{self.initials()} is not a senior."
+
+
 user1 = User("John", "Doe", 19)
 user1.print()
-print(user1.initials())
+print(user1.isSenior())
+
+user2 = User("Mary", "Jane", 66)
+user2.print()
+print(user2.isSenior())
