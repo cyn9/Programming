8857f6e25de8988af63e9d1aa96e9357b4084f52
cyn9 <cihan.asci@tufts.edu>
2020-10-25 23:15:37 -0400

    Center window function added.

--- Python/gui_app_2.py
+++ Python/gui_app_2.py
@@ -2,16 +2,24 @@ import tkinter as tk
 import tkinter as tk
 from tkinter import LabelFrame
 
+def centerWindow(width = 600, height = 600):
+    ws = root.winfo_screenwidth()
+    hs = root.winfo_screenheight()
+
+    x = ws/2 - width/2
+    y = hs/2 - height/2
+    root.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
+
 root = tk.Tk()
+centerWindow(600, 600)
 
 antennaGeometry = LabelFrame(root,
                              text = "Antenna Geometry",
                              height = 200,
                              width = 300,
                              borderwidth = 3)
-antennaGeometry.pack(fill = "both", 
-                     expand = "Yes",
-                     padx = 5,
+antennaGeometry.pack(padx = 5,
                      pady = 5)
 
 
