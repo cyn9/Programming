0f2bed0bf805c6aadbdc6c5c6fe93a5d989904bf
cyn9 <cihan.asci@tufts.edu>
2020-10-26 00:50:55 -0400

    Added MoM panel.

--- Python/gui_app_2.py
+++ Python/gui_app_2.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 
+from tkinter import Tk
 from tkinter import Label
 from tkinter import LabelFrame
 from tkinter import Entry
+from tkinter import OptionMenu
+from tkinter import StringVar
 
 appWidth = 350
-appHeight = 400
+appHeight = 435
 
 def centerWindow(width = appWidth, height = appHeight):
     ws = root.winfo_screenwidth()
@@ -15,20 +18,39 @@ def centerWindow(width = appWidth, height = appHeight):
     y = hs/2 - height/2
     root.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
-
-root = tk.Tk()
+# # # # # # # # # # # # # #
+# Root window properties  #
+# # # # # # # # # # # # # #
+root = Tk()
 root.title("Antenna Design Tool")
 root.option_add("*Font", "Arial")
 root.resizable(0, 0)
-
-antennaLen = tk.StringVar()
-wireRadius = tk.StringVar()
-numOfLoads = tk.StringVar()
-startFreq = tk.StringVar()
-stopFreq = tk.StringVar()
-freqStep = tk.StringVar()
-
 centerWindow(appWidth, appHeight)
+
+# # # # # # # # # # #
+# Widget Variables  #
+# # # # # # # # # # #
+antennaLen = StringVar()
+wireRadius = StringVar()
+numOfLoads = StringVar()
+startFreq = StringVar()
+stopFreq = StringVar()
+freqStep = StringVar()
+numOfSegments = StringVar(root, value = 101)
+
+BASIS_FUNC_OPTIONS = [ "Pulse Function",
+                       "Piecewise Linear",
+                       "Piecewise Sinusoidal" ]
+
+WIRE_EXCITATION_OPTIONS = [ "Delta-Gap",
+                            "Magnetic Frill",
+                            "Plane Wave" ]
+
+basisFunc = StringVar(root)
+basisFunc.set(BASIS_FUNC_OPTIONS[0])
+
+wireExcitation = StringVar(root)
+wireExcitation.set(WIRE_EXCITATION_OPTIONS[0])
 
 # # # # # # # # #
 # Label frames  #
@@ -37,19 +59,26 @@ antennaGeometry = LabelFrame(root,
                              text = "Antenna Geometry",
                              font = ("Arial", 12, "bold"),
                              height = 130,
-                             width = 300,
+                             width = 325,
                              borderwidth = 3)
-antennaGeometry.pack(padx = 5,
-                     pady = 5)
 
 frequencyFrame = LabelFrame(root,
                             text = "Frequency",
                             font = ("Arial", 12, "bold"),
                             height = 130,
-                            width = 300,
+                            width = 325,
                             borderwidth = 3)
-frequencyFrame.pack(padx = 5,
-                     pady = 5)
+
+MoMFrame = LabelFrame(root,
+                      text = "Method of Moments",
+                      font = ("Arial", 12, "bold"),
+                      height = 130,
+                      width = 325,
+                      borderwidth = 3)
+
+antennaGeometry.pack(padx = 5, pady = 5)
+frequencyFrame.pack(padx = 5, pady = 5)
+MoMFrame.pack(padx = 5, pady = 5)
 
 # # # # # #
 # Labels  #
@@ -84,51 +113,94 @@ lbl_FreqStep = Label(frequencyFrame,
                      padx = 3, 
                      pady = 3)
 
+lbl_NumberOfSegments = Label(MoMFrame, 
+                             text = "Number of segments : ",
+                             padx = 3,
+                             pady = 3)
+
+lbl_BasisFunction = Label(MoMFrame, 
+                          text = "Basis function : ",
+                          padx = 3,
+                          pady = 3)
+
+lbl_Excitation = Label(MoMFrame, 
+                       text = "Wire Excitation : ",
+                       padx = 3,
+                       pady = 3)
+
 # # # # # # # #
 # Text Fields #
 # # # # # # # #
 txt_MonopoleLength = Entry(antennaGeometry,
-                           width = 12,
+                           width = 15,
                            text = "",
+                           justify = "center",
                            font = ('Arial', 12),
                            textvariable = antennaLen)
 
 txt_WireRadius = Entry(antennaGeometry,
-                       width = 12,
+                       width = 15,
                        text = "",
+                       justify = "center",
                        font = ('Arial', 12),
                        textvariable = wireRadius)
 
 txt_NumberOfLoads = Entry(antennaGeometry,
-                          width = 12,
+                          width = 15,
                           text = "",
+                          justify = "center",
                           font = ('Arial', 12),
                           textvariable = numOfLoads)
 
 txt_StartFreq = Entry(frequencyFrame,
-                      width = 12,
+                      width = 15,
                       text = "",
+                      justify = "center",
                       font = ('Arial', 12),
                       textvariable = startFreq)
 
 txt_StopFreq = Entry(frequencyFrame,
-                     width = 12,
+                     width = 15,
                      text = "",
+                     justify = "center",
                      font = ('Arial', 12),
                      textvariable = stopFreq)
 
 txt_FreqStep = Entry(frequencyFrame,
-                     width = 12,
+                     width = 15,
                      text = "",
+                     justify = "center",
                      font = ('Arial', 12),
                      textvariable = freqStep)
+
+txt_NumberOfSegments = Entry(MoMFrame,
+                             width = 15,
+                             text = "",
+                             justify = "center",
+                             font = ('Arial', 12),
+                             textvariable = numOfSegments)
+
+# # # # # # #
+# Dropdowns #
+# # # # # # #
+opt_BasisFunc = OptionMenu(MoMFrame,
+                           basisFunc,
+                           *BASIS_FUNC_OPTIONS)
+opt_BasisFunc.config(width = 16,
+                     font = "Arial 8")
+
+opt_WireExcitation = OptionMenu(MoMFrame,
+                                wireExcitation,
+                                *WIRE_EXCITATION_OPTIONS)
+opt_WireExcitation.config(width = 16,
+                          font = "Arial 8")
 
 # # # # # # # # # # # # #
 # Placement of widgets  #
 # # # # # # # # # # # # #
-lbl_MonopoleLength.place(x = 0, y = 5)
-lbl_WireRadius.place(x = 21, y = 35)
-lbl_NumberOfLoads.place(x = 33, y = 65)
+lbl_MonopoleLength.place(x = 9, y = 5)
+lbl_WireRadius.place(x = 30, y = 35)
+lbl_NumberOfLoads.place(x = 42, y = 65)
 txt_MonopoleLength.place(x = 172, y = 9)
 txt_WireRadius.place(x = 172, y = 39)
 txt_NumberOfLoads.place(x = 172, y = 69)
@@ -139,5 +211,12 @@ lbl_FreqStep.place(x = -0.4, y = 65)
 txt_StartFreq.place(x = 172, y = 9)
 txt_StopFreq.place(x = 172, y = 39)
 txt_FreqStep.place(x = 172, y = 69)
+
+lbl_NumberOfSegments.place(x = 10, y = 5)
+lbl_BasisFunction.place(x = 56.7, y = 35)
+lbl_Excitation.place(x = 49, y = 65)
+txt_NumberOfSegments.place(x = 172, y = 9)
+opt_BasisFunc.place(x = 170, y = 36)
+opt_WireExcitation.place(x = 170, y = 65)
 
 root.mainloop()
