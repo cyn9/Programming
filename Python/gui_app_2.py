9683491580b56a5dbeb7a0472856029c25476b25
cyn9 <cihan.asci@tufts.edu>
2020-10-25 23:22:23 -0400

    Added some labels and root window properties.

--- Python/gui_app_2.py
+++ Python/gui_app_2.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-import tkinter as tk
+
+from tkinter import Label
 from tkinter import LabelFrame
 
 def centerWindow(width = 600, height = 600):
@@ -12,6 +13,9 @@ def centerWindow(width = 600, height = 600):
 
 
 root = tk.Tk()
+root.option_add("*Font", "Arial")
+root.resizable(0, 0)
+
 centerWindow(600, 600)
 
 antennaGeometry = LabelFrame(root,
@@ -22,6 +26,13 @@ antennaGeometry = LabelFrame(root,
 antennaGeometry.pack(padx = 5,
                      pady = 5)
 
+lbl_MonopoleLength = Label(antennaGeometry, text = "Monopole Length (m) : ")
+lbl_MonopoleLength.place(x = 0, y = 5)
 
+lbl_WireRadius = Label(antennaGeometry, text = "Wire Radius (mm) : ")
+lbl_WireRadius.place(x = 0, y = 30)
+
+lbl_NumberOfLoads = Label(antennaGeometry, text = "Number of loads : ")
+lbl_NumberOfLoads.place(x = 0, y = 55)
 
 root.mainloop()
