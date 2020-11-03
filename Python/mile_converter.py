d421936e70204640a0ee35f7251746b34bf627c0
cyn9 <cihan.asci@tufts.edu>
2020-11-03 11:38:51 -0500

    Deleted some files.

--- Python/mile_converter.py
+++
@@ -1,6 +0,0 @@
-kms = input("How many kilometers did you run today? >> ")
-
-miles = round(float(kms) / 1.609344, 2)
-
-print(f"Your {kms} km run was {miles} mi today.")
-print("Your " + kms + " km run was " + str(miles) + " mi today.")
