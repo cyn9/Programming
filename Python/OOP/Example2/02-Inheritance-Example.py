d3405a7556774904f10ae49c0e098c7125221364
cyn9 <cihan.asci@tufts.edu>
2020-11-11 18:40:24 -0500

    Added a few decorators.

--- Python/OOP/Example2/02-Inheritance-Example.py
+++ Python/OOP/Example2/02-Inheritance-Example.py
@@ -1,5 +1,6 @@
 class Human:
-    def __init__(self, firstName, lastName, age) -> None:
+    def __init__(self, firstName, lastName, age):
+        # Internal class variables
         self._firstName = firstName
         self._lastName = lastName
         self._age = age
@@ -24,6 +25,14 @@ class Human:
 
     # Using decorators:
     @property
+    def fullName(self):
+        return f"Full name : {self._firstName} {self._lastName}"
+
+    @fullName.setter
+    def fullName(self, fullName):
+        self._firstName, self._lastName = fullName.split(" ")
+
+    @property
     def age(self):
         return self._age
     
@@ -37,3 +46,11 @@ class Human:
 
 person1 = Human("John", "Doe", 30)
 print(person1.age)
+
+person1.age = 32
+print(person1.age)
+
+print(person1.fullName)
+
+person1.fullName = "Jane Doe"
+print(person1.fullName)
