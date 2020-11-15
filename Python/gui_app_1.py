c3d20e79b56920d21422580fdda9ed866213bc20
cyn9 <cihan.asci@tufts.edu>
2020-11-15 12:15:41 -0500

    Folder cleanup.

--- Python/gui_app_1.py
+++
@@ -1,8 +0,0 @@
-import wx
-
-app = wx.App()
-
-frame = wx.Frame(None, title = "Simple Application")
-frame.Show()
-
-app.MainLoop()
--- Python/repeater_loop.py
+++
@@ -1,4 +0,0 @@
-number = input("How many times do I have to tell you? >> ")
-
-for i in range(int(number)):
-	print(f"{i+1} : Clean up your room!".upper())
