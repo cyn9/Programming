9550f08fa256affc48e65c8074087492f0f78244
cyn9 <cihan.asci@tufts.edu>
2020-11-23 12:46:11 -0500

    Added an example on access modifiers.

---
+++ C++/07-Inheritance/02-Access-Modifiers.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class Base {
+
+public:
+    int a;
+    
+    Base();
+    ~Base();
+
+    void display();
+
+    int getA() const { return this->a; }
+    int getB() const { return this->b; }
+    int getC() const { return this->c; }
+
+    void setA(int a) { this->a = a; }
+    void setB(int b) { this->b = b; }
+    void setC(int c) { this->c = c; }
+
+protected:
+    int b;
+
+private:
+    int c;
+
+};
+
+class Derived : public Base {
+    
+    // a is accessible.
+    // b is protected.
+    // c is not accessible.
+
+public:
+    Derived();
+    ~Derived();
+
+    void changeBaseMembers();
+    void printBaseMembers();
+
+};
+
+Base::Base() : a{0}, b{0}, c{0} {}
+Base::~Base()                   {}
+
+Derived::Derived() {}
+Derived::~Derived() {}
+
+void Base::display() { cout << "a : " << a << ", b : " << b << ", c : " << c << endl; }
+
+void Derived::changeBaseMembers()   { this->a = 5; this->b = 3; }
+void Derived::printBaseMembers()    { cout << "a : " << a << ", b : " << b << endl; }
+
+int main() {
+    Base b;
+    b.display();
+
+    Derived d;
+    d.printBaseMembers();
+    d.changeBaseMembers();
+    d.printBaseMembers();
+
+    return 0;
+}
