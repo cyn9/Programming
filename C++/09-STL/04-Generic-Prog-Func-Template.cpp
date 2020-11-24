f1e99c3eaa4f14be971b77dcae30d55304e14090
cyn9 <cihan.asci@tufts.edu>
2020-11-24 01:15:35 -0500

    Added function overloading for "<<".

--- C++/09-STL/04-Generic-Prog-Func-Template.cpp
+++ C++/09-STL/04-Generic-Prog-Func-Template.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 template <typename T> T min(T, T);
+template <typename T1, typename T2> void Foo(T1, T2);
 
 struct Person {
     std::string name;
@@ -18,6 +19,14 @@ struct Person {
 
 };
 
+// Overloading "<<" stream operator in order
+// to be able to use inside Foo() function
+// that includes "cout <<".
+std::ostream &operator<<(std::ostream &os, const Person &obj) {
+    os << obj.name << " " << obj.age << std::endl;
+    return os;
+}
+
 int main() {
 
     Person p1 {"John Doe", 32};
@@ -25,10 +34,17 @@ int main() {
 
     std::cout << min(p1, p2).name << " is younger." << std::endl;
 
+    Foo(p1, p2);
+
     return 0;
 }
 
 template <typename T>
 T min(T a, T b) {
     return ((a < b) ? a : b);
+}
+
+template <typename T1, typename T2>
+void Foo(T1 a, T2 b) {
+    std::cout << a << b << std::endl;
 }
