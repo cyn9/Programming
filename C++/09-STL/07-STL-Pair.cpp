0796a4f89935db5cafa4ab725d18ef0606014098
cyn9 <cihan.asci@tufts.edu>
2020-11-29 17:12:33 -0500

    Built-in pair<> from STL.

---
+++ C++/09-STL/07-STL-Pair.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+
+int main() {
+
+    std::pair<int, double>        p1 {5, 3.45};
+    std::pair<std::string, float> p2 {"Hello, C++", 3.45f};
+    std::pair<int, int>           p3 {10, 20};
+
+    std::cout << "<" << p1.first << ", " << p1.second << ">" << std::endl;
+    std::cout << "<" << p2.first << ", " << p2.second << ">" << std::endl;
+    std::cout << "<" << p3.first << ", " << p3.second << ">" << std::endl;
+
+
+    return 0;
+}
