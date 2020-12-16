a94aae2684945c4d186634c897679ca975ca0d16
cyn9 <cihan.asci@tufts.edu>
2020-12-16 02:24:03 -0500

    Pass char array to a func. using a func pointer.

---
+++ C++/02-Pointers/04-Pass-Char-Array-Function-Pointer.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <memory>
+
+void print(const char *, unsigned);
+// void print(std::string, unsigned);
+
+int main() {
+    
+    constexpr char *ch = "Hello, C++.";
+    void (*fnc_ptr) (const char *, unsigned) = &print;
+    
+    (*fnc_ptr)(ch, 6);
+    
+    return 0;
+}
+
+void print(const char* ch, unsigned n) {
+    int* i = new int(0);
+    size_t sz {0};
+    
+    for (; ch[*i] != '\0'; (*i)++) { sz++; }
+    
+    delete i;
+    i = nullptr;
+    
+    for (unsigned i = 0; i < n; i++) {
+        for (size_t j = 0; j < sz; j++)
+            std::cout << ch[j];
+        
+        std::cout << '\n';
+    }
+    
+    std::cout << std::endl;
+}
